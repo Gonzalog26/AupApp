59008dd146bc31852f190c6a9cfe7e9a02ecba01
Gonzalo Gil <gonzalog26gil@gmail.com>
2017-12-27 13:40:55 +0100

    Hecha actividad inicial con transicion a la siguiente e implementacion de idiomas

--- app/src/main/java/eus/ehu/tta/aupapp/MainActivity.java
+++ app/src/main/java/eus/ehu/tta/aupapp/MainActivity.java
@@ -1,7 +1,12 @@
 package eus.ehu.tta.aupapp;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void castellano(View view){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+    public void ingles(View view){
+        Locale localizacion = new Locale("en","En");
+        Locale.setDefault(localizacion);
+        Configuration conf = new Configuration();
+        conf.locale = localizacion;
+        getBaseContext().getResources().updateConfiguration(conf,getBaseContext().getResources().getDisplayMetrics());
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+    public void frances(View view){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
 }
