1b3871f594e68860120bf124ee0c1aee96fcc425
Gonzalo Gil <gonzalog26gil@gmail.com>
2017-12-29 12:55:38 +0100

    Actividad menu de ver o crear eventos hecha

--- app/src/main/java/eus/ehu/tta/aupapp/EventosMenuActivity.java
+++ app/src/main/java/eus/ehu/tta/aupapp/EventosMenuActivity.java
@@ -1,7 +1,9 @@
 package eus.ehu.tta.aupapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EventosMenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class EventosMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eventos_menu2);
+    }
+
+    public void crearEvento(View view){
+        Intent intent = new Intent(this,CrearEventoActivity.class);
+        startActivity(intent);
+    }
+
+    public void verEventos(View view){
+        Intent intent = new Intent(this, VerEventosActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/eus/ehu/tta/aupapp/VerEventosActivity.java
@@ -0,0 +1,13 @@
+package eus.ehu.tta.aupapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerEventosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_eventos);
+    }
+}
