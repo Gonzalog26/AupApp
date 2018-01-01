3e6ecf69d91ae16a0ab8a0e731e6d5979018f629
Gonzalo Gil <gonzalog26gil@gmail.com>
2018-01-01 20:20:05 +0100

    trabajando en las actividades y layouts de crear eventos y verlos

--- app/src/main/java/eus/ehu/tta/aupapp/CrearEventoActivity.java
+++ app/src/main/java/eus/ehu/tta/aupapp/CrearEventoActivity.java
@@ -1,7 +1,9 @@
 package eus.ehu.tta.aupapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CrearEventoActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class CrearEventoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_evento);
+    }
+
+    public void crearEvento(View view){
+
+        //Funciones necesarias para crear el evento
+
+
+        //Una vez creado el evento se vuelve al menu inicial
+        Intent intent = new Intent(this,MenuActivity.class);
+        startActivity(intent);
+    }
+
+    public void sacarFoto(View view){
+
+        //Funciones necesarias para sacar una foto
+
     }
 }
--- app/src/main/java/eus/ehu/tta/aupapp/VerEventosActivity.java
+++ app/src/main/java/eus/ehu/tta/aupapp/VerEventosActivity.java
@@ -2,6 +2,7 @@ package eus.ehu.tta.aupapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class VerEventosActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class VerEventosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_eventos);
+    }
+
+    public void verEventos(View view){
+
+        //Funcion para crear la View con los eventos entre las fechas indicadas
+
     }
 }
