e04be9ca9a18d9abf632f81d4b5d5bb60bb95097
Gonzalo Gil <gonzalog26gil@gmail.com>
2018-01-26 11:50:42 +0100

    Acabando actividad verEventos (Falta la parte de google maps)

--- app/src/main/java/eus/ehu/tta/aupapp/VerEventosActivity.java
+++ app/src/main/java/eus/ehu/tta/aupapp/VerEventosActivity.java
@@ -11,6 +11,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -28,7 +29,7 @@ import eus.ehu.tta.aupapp.modelo.User;
 import eus.ehu.tta.aupapp.negocio.ProgressTask;
 import eus.ehu.tta.aupapp.negocio.ServidorNegocio;
 
-public class VerEventosActivity extends AppCompatActivity {
+public class VerEventosActivity extends AppCompatActivity implements View.OnClickListener{
 
 
     ServidorNegocio servidorNegocio = ServidorNegocio.getInstancia();
@@ -47,8 +48,8 @@ public class VerEventosActivity extends AppCompatActivity {
         /*final int fechaInicial = Integer.parseInt(((EditText)findViewById(R.id.fecha_inicial)).getText().toString());
         final int fechaFinal = Integer.parseInt(((EditText)findViewById(R.id.fecha_final)).getText().toString());*/
 
-        final int fechaInicial = 20171220;
-        final int fechaFinal = 20171221;
+        final int fechaInicial = 20171200;
+        final int fechaFinal = 20180100;
 
         new ProgressTask<List<Event>>(this){
 
@@ -60,50 +61,68 @@ public class VerEventosActivity extends AppCompatActivity {
             @Override
             protected void onFinish(List<Event> result){
 
-                LinearLayout linearLayout = (LinearLayout) findViewById(R.id.layout);
-
-                Button button = (Button)findViewById(R.id.boton_ubicacion);
+                LinearLayout linearLayout = (LinearLayout) findViewById(R.id.layout_principal);
 
                 linearLayout.removeAllViews();
+                linearLayout.setGravity(View.TEXT_ALIGNMENT_CENTER);
+
 
                 for(int i=0;i<result.size();i++){
 
                     TextView textView = new TextView(context);
                     textView.setText(result.get(i).getNombre());
                     textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP,25);
-                    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                    layoutParams.setMargins(0,0,0,50);
-                    layoutParams.gravity=Gravity.CENTER;
-                    textView.setLayoutParams(layoutParams);
+                    LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+                    layoutParams1.setMargins(50,100,50,100);
+                    layoutParams1.gravity=Gravity.CENTER_HORIZONTAL; //NO lo hace
+                    textView.setLayoutParams(layoutParams1);
                     textView.setTypeface(null, Typeface.BOLD);
                     linearLayout.addView(textView);
 
                     ImageView imageView = new ImageView(context);
-                    Picasso.with(context).load(baseUrl+result.get(i).getFoto()).resize(1000,1000).into(imageView);
+                    Picasso.with(context).load(baseUrl+result.get(i).getFoto()).resize(400,400).into(imageView);
                     linearLayout.addView(imageView);
 
                     textView = new TextView(context);
-                    textView.setText("Fecha: "+result.get(i).getFecha());
+                    LinearLayout.LayoutParams layoutParams2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+                    layoutParams2.setMargins(50,100,50,0);
+                    textView.setLayoutParams(layoutParams2);
+                    Integer fecha = result.get(i).getFecha();
+                    String fecha2 = fecha.toString().substring(6,8)+"/"+fecha.toString().substring(4,6)+"/"+fecha.toString().substring(0,4);
+                    textView.setText("Fecha: "+fecha2);
                     linearLayout.addView(textView);
 
                     textView = new TextView(context);
-                    textView.setText("Hora: "+result.get(i).getHora());
+                    LinearLayout.LayoutParams layoutParams3 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+                    layoutParams3.setMargins(50,0,50,0);
+                    textView.setLayoutParams(layoutParams3);
+                    Integer hora = result.get(i).getHora();
+                    String hora2;
+                    if(hora<1000){
+                        hora2 = hora.toString().substring(0,1)+":"+hora.toString().substring(1,3);
+                    }else{
+                        hora2 = hora.toString().substring(0,2)+":"+hora.toString().substring(2,4);
+                    }
+
+                    textView.setText("Hora: "+hora2);
                     linearLayout.addView(textView);
 
                     textView = new TextView(context);
+                    textView.setLayoutParams(layoutParams3);
                     textView.setText("Descripcion: "+result.get(i).getDescripcion());
                     linearLayout.addView(textView);
 
-                    /*button.setVisibility(View.VISIBLE);
-                    linearLayout.addView(button);*/
+                    Button button = new Button(context);
+                    button.setId(i);
+                    LinearLayout.LayoutParams layoutParams4 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    button.setLayoutParams(layoutParams4);
+                    layoutParams4.setMargins(350,30,0,0);
+                    button.setText(R.string.ver_ubicacion);
+                    linearLayout.addView(button);
 
-                    /*layoutParams.gravity=Gravity.NO_GRAVITY;
-                    textView.setLayoutParams(layoutParams);*/
 
 
                 }
-
-
 
 
             }
@@ -117,4 +136,13 @@ public class VerEventosActivity extends AppCompatActivity {
         }.execute();
 
     }
+
+    public void onClick(View view){
+
+
+
+    }
+
 }
+
+
